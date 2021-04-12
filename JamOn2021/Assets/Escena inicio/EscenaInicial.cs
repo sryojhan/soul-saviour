dda539d74f9014b5c7c28a5ed4dc35f2823d5a0b
Yojhan <sryojhan@gmail.com>
2021-04-12 15:09:04 +0200

    Creación del proyecto

---
+++ JamOn2021/Assets/Escena inicio/EscenaInicial.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EscenaInicial : MonoBehaviour
+{
+    static string[] NAMES = { "Yojhan Steven García Peña", "Iván Sánchez Míguez", "Pablo Fernández Álvarez" };
+
+    public Transform logo;
+    public GameObject yojhanNoSabeHacerTortitas;
+
+    public GameObject title;
+    public Text groupMembers;
+
+    public float logoTime = 1;
+    public float aGameByTime = 1;
+
+
+    private float t = 0;
+    bool sceneSwap = false;
+    private void Update()
+    {
+        if (!sceneSwap)
+        {
+            if ((t += Time.deltaTime) > logoTime)
+            {
+                Change();
+            }
+        }
+        else
+        {
+            if ((t += Time.deltaTime) > aGameByTime)
+            {
+                SceneManager.LoadScene((int)Escenas.Menu);
+            }
+        }
+    }
+
+
+    void Change()
+    {
+        t = 0;
+        sceneSwap = true;
+
+        logo.gameObject.SetActive(false);
+        yojhanNoSabeHacerTortitas.SetActive(false);
+
+        title.SetActive(true);
+        groupMembers.gameObject.SetActive(true);
+        createText();
+    }
+
+    void createText()
+    {
+        int[] orden = { 0, 1, 2 };
+        for (int i = 0; i < 3; i++)
+        {
+            int random = Random.Range(0, 3);
+            int c = orden[random];
+            orden[random] = orden[i];
+            orden[i] = c;
+        }
+
+        groupMembers.text = "";
+        for (int i = 0; i < 3; i++)
+        {
+            groupMembers.text += NAMES[orden[i]] + "\n";
+        }
+    }
+}
