55445652df4739e79470aaaba66fe3700394d6f1
Yojhan <sryojhan@gmail.com>
2021-04-15 23:50:47 +0200

    Revert ":("
    This reverts commit 68b40846095d3ddb241616a24d4635c4af0832f9.

--- JamOn2021/Assets/Scripts/PlayerMovement.cs
+++ JamOn2021/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
 
     bool aDown = false, sDown = false, wDown = false, dDown = false, awDown = false, asDown = false, wdDown = false, sdDown = false;
 
+   // float x = 0;
+    //float y = 0;
     void Start() { 
         rb = GetComponent<Rigidbody2D>();
         dash = GetComponent<PlayerDash>();
@@ -20,7 +22,10 @@ public class PlayerMovement : MonoBehaviour
         aDown = false; sDown = false; wDown = false; dDown = false;  awDown = false; asDown = false; wdDown = false; sdDown = false;
         if (!dash.enabled)
         {
-            if (Input.GetKeyDown(KeyCode.Space)) dash.enabled = true;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                dash.enabled = true;
+            }
             else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W)) awDown = true;
             else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S)) asDown = true;
             else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)) wdDown = true;
@@ -30,12 +35,28 @@ public class PlayerMovement : MonoBehaviour
             else if (Input.GetKey(KeyCode.W)) wDown = true;
             else if (Input.GetKey(KeyCode.D)) dDown = true;
 
+          //  x = Input.GetAxis("Horizontal");
+         //   y = Input.GetAxis("Vertical");
         }
     }
     private void FixedUpdate()
     {
         if (!dash.enabled)
         {
+      //      rb.velocity = new Vector2(x, y).normalized * speed;
+
+
+            //DASH CON UTLIMA DIRECCION TOMANDA
+            /*if (awDown) { rb.velocity = (Vector2.left + Vector2.up).normalized * speed * Time.fixedDeltaTime; direction = rb.velocity.normalized; }
+            else if (asDown) { rb.velocity = (Vector2.left + Vector2.down).normalized * speed * Time.fixedDeltaTime; direction = rb.velocity.normalized; }
+            else if (wdDown) { rb.velocity = (Vector2.right + Vector2.up).normalized * speed * Time.fixedDeltaTime; direction = rb.velocity.normalized; }
+            else if (sdDown) { rb.velocity = (Vector2.right + Vector2.down).normalized * speed * Time.fixedDeltaTime; direction = rb.velocity.normalized; }
+            else if (aDown) { rb.velocity = Vector2.left * speed * Time.fixedDeltaTime; direction = rb.velocity.normalized; }
+            else if (wDown) { rb.velocity = Vector2.up * speed * Time.fixedDeltaTime; direction = rb.velocity.normalized; }
+            else if (sDown) { rb.velocity = Vector2.down * speed * Time.fixedDeltaTime; direction = rb.velocity.normalized; }
+            else if (dDown) { rb.velocity = Vector2.right * speed * Time.fixedDeltaTime; direction = rb.velocity.normalized; }
+            else { rb.velocity = Vector2.zero; direction = rb.velocity.normalized; }*/
+
             if (awDown) rb.velocity = (Vector2.left + Vector2.up).normalized * speed;
             else if (asDown) rb.velocity = (Vector2.left + Vector2.down).normalized * speed; 
             else if (wdDown) rb.velocity = (Vector2.right + Vector2.up).normalized * speed;
