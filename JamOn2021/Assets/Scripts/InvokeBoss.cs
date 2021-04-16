d5462d3a57977b60e827c327e428c3348f7d7137
ivasan07 <ivasan07@ucm.es>
2021-04-17 00:53:45 +0200

    torch logic

---
+++ JamOn2021/Assets/Scripts/InvokeBoss.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeBoss : MonoBehaviour
+{
+    [SerializeField] GameObject[] torches;
+    [SerializeField] GameObject boss;
+
+    [SerializeField] Sprite lightedTorch;
+
+    int numTorches;
+
+    int indexAux = 0;
+
+    private void Start()
+    {
+        numTorches = torches.Length;
+    }
+
+    public void lightTorch()
+    {
+        if (indexAux < torches.Length)
+        {
+            numTorches--;
+            torches[indexAux].GetComponent<SpriteRenderer>().sprite = lightedTorch;
+            indexAux++;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) lightTorch();
+    }
+
+    void invokeBoss()
+    {
+        print("invoke boss");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (numTorches <= 0)
+        {
+            if (collision.GetComponent<PlayerSweepAttack>())
+            {
+                invokeBoss();
+            }
+        }
+    }
+
+}
