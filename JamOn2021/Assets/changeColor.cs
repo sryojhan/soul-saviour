85eebe87ef549305d498c9df158b162df6cccaa5
PabloFernandezAl01 <pablfe20@ucm.es>
2021-04-18 22:30:06 +0200

    commit

--- JamOn2021/Assets/changeColor.cs
+++ JamOn2021/Assets/changeColor.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 
 public class changeColor : MonoBehaviour
 {
+    public Slider pepe;
+    public Image[] images;
     Image image;
     private void Start()
     {
+        pepe.value = ColorPalette.currentHue;
         image = GetComponent<Image>();
-        image.color = Color.HSVToRGB(ColorPalette.currentHue, 1, 1);
+        cambiacoloratodo(Color.HSVToRGB(ColorPalette.currentHue, 1, 1));
     }
     public void colorChange(float value)
     {
         ColorPalette.currentHue = value;
-        if(image)
-        image.color = Color.HSVToRGB(value, 1, 1);
+        if(image) cambiacoloratodo(Color.HSVToRGB(value, 1, 1));
+    }
+
+    void cambiacoloratodo(Color a)
+    {
+        for(int i = 0; i< images.Length; i++)
+        {
+            images[i].color = a;
+        }
+        image.color = a;
     }
 }
