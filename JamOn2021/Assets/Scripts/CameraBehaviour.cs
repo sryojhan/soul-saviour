f702473d54acd633e6355600c545d28fee7de205
yojhansg <yojhansg@ucm.es>
2021-04-18 18:15:49 +0200

    xhNF

--- JamOn2021/Assets/Scripts/CameraBehaviour.cs
+++ JamOn2021/Assets/Scripts/CameraBehaviour.cs
@@ -4,6 +4,7 @@ public class CameraBehaviour : MonoBehaviour
 {
     [SerializeField] Transform playerTr;
     Vector3 cameraPosition;
+    Vector3 vel = Vector3.zero;
     void Start()
     {
         setCameraPosition();
@@ -15,7 +16,8 @@ public class CameraBehaviour : MonoBehaviour
     }
     void setCameraPosition()
     {
-        cameraPosition = playerTr.position;
+        cameraPosition = Vector3.SmoothDamp(transform.position, playerTr.position, ref vel, 0.3f);
+        //cameraPosition = playerTr.position;
         cameraPosition.z = transform.position.z;
         transform.position = cameraPosition;
     }
